4f9f39da987b8af3cb4971e1344fc9d978ca0520
roman <damnfker@gmail.com>
2025-06-08 14:59:56 +0200

    fixed app logic

--- zyfra_test/zyfra_app/models.py
+++ zyfra_test/zyfra_app/models.py
@@ -4,7 +4,7 @@ from shapely.geometry import Point, Polygon
 
 class SamosvalDescription(models.Model):
     name = models.CharField(max_length=50, unique=True)
-    max_capacity = models.FloatField()
+    max_capacity = models.IntegerField()
 
     def __str__(self):
         return self.name
@@ -14,11 +14,13 @@ class Ruda(models.Model):
     sio2 = models.FloatField()
     fe = models.FloatField()
 
+    def __str__(self):
+        return f"SiO2: {self.sio2:.2f}%, Fe: {self.fe:.2f}%"
 
 class Samosval(models.Model):
     number = models.CharField(max_length=10, unique=True)
     model = models.ForeignKey(SamosvalDescription, on_delete=models.CASCADE)
-    current_load = models.FloatField()
+    current_load = models.IntegerField()
     ruda = models.ForeignKey(Ruda, on_delete=models.CASCADE)
 
     def peregruz_percent(self):
@@ -35,7 +37,7 @@ class Samosval(models.Model):
 
 class Warehouse(models.Model):
     name = models.CharField(max_length=50, default="Склад")
-    current_quantity = models.FloatField(default=0)
+    current_quantity = models.IntegerField(default=0)
     ruda = models.ForeignKey(Ruda, on_delete=models.CASCADE)
     polygon_wkt = models.TextField()
 
@@ -44,15 +46,13 @@ class Warehouse(models.Model):
         point = Point(x, y)
         return polygon.contains(point) or polygon.boundary.contains(point)
 
-    def update_ruda(self, truck):
-        total_mass = self.current_quantity + truck.current_load
+    def update_ruda(self, samosval):
+        total_mass = self.current_quantity + samosval.current_load
         if total_mass > 0:
-            sio2 = ((self.ruda.sio2_percentage * self.current_quantity + truck.ore.sio2_percentage * truck.current_load)
-                    / total_mass)
-            fe = ((self.ruda.fe_percentage * self.current_quantity + truck.ore.fe_percentage * truck.current_load)
-                  / total_mass)
-            self.ruda.sio2_percentage = sio2
-            self.ruda.fe_percentage = fe
+            sio2 = ((self.ruda.sio2 * self.current_quantity + samosval.ruda.sio2 * samosval.current_load) / total_mass)
+            fe = ((self.ruda.fe * self.current_quantity + samosval.ruda.fe * samosval.current_load) / total_mass)
+            self.ruda.sio2 = sio2
+            self.ruda.fe = fe
             self.current_quantity = total_mass
             self.ruda.save()
             self.save()
--- zyfra_test/zyfra_app/views.py
+++ zyfra_test/zyfra_app/views.py
@@ -5,20 +5,24 @@ from .models import Samosval, Warehouse
 def index(request):
     samosvals = Samosval.objects.all()
     warehouse = Warehouse.objects.first()
+    initial_quantity = warehouse.current_quantity
 
     if request.method == 'POST':
         for samosval in samosvals:
-            coords = request.POST.get(f'coords_{samosvals.id}', '')
+            coords = request.POST.get(f'coords_{samosval.id}', '')
             if coords:
                 try:
                     x, y = map(float, coords.split())
                     if warehouse.is_point_inside(x, y):
-                        warehouse.update_ore(samosval)
+                        warehouse.update_ruda(samosval)
                 except ValueError:
                     pass
 
     context = {
         'samosvals': samosvals,
         'warehouse': warehouse,
+        'initial_quantity': initial_quantity,
     }
+
+
     return render(request, 'index.html', context)
